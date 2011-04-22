d04bb38dc9be38a0c917e679527a6a5eb2bdd608
Mike Doherty <doherty@cpan.org>
2011-04-22 04:48:38 +0000

    Import of DOHERTY/Pod-Weaver-PluginBundle-Author-DOHERTY-0.005 from CPAN.
    gitpan-cpan-distribution: Pod-Weaver-PluginBundle-Author-DOHERTY
    gitpan-cpan-version:      0.005
    gitpan-cpan-path:         DOHERTY/Pod-Weaver-PluginBundle-Author-DOHERTY-0.005.tar.gz
    gitpan-cpan-author:       DOHERTY
    gitpan-cpan-maturity:     released

--- lib/Pod/Weaver/PluginBundle/Author/DOHERTY.pm
+++ lib/Pod/Weaver/PluginBundle/Author/DOHERTY.pm
@@ -1,16 +1,13 @@
+package Pod::Weaver::PluginBundle::Author::DOHERTY;
+# ABSTRACT: Pod::Weaver configuration the way DOHERTY does it
 use strict;
 use warnings;
-
-package Pod::Weaver::PluginBundle::Author::DOHERTY;
-BEGIN {
-  $Pod::Weaver::PluginBundle::Author::DOHERTY::VERSION = '0.004';
-}
-# ABSTRACT: Pod::Weaver configuration the way DOHERTY does it
+our $VERSION = '0.005'; # VERSION
 
 # Dependencies
 use Pod::Weaver::Plugin::Encoding                       qw();
 use Pod::Weaver::Section::Availability       1.102570   qw(); # To set github URLs properly: https://rt.cpan.org/Ticket/Display.html?id=63364
-use Pod::Weaver::Section::SourceGitHub 0.53             qw();
+use Pod::Weaver::Section::SourceGitHub       0.53       qw();
 use Pod::Weaver::Section::BugsAndLimitations 1.102670   qw(); # To read the bugtracker properly: https://rt.cpan.org/Ticket/Display.html?id=61605
 
 use Pod::Weaver::Config::Assembler;
@@ -57,7 +54,7 @@ Pod::Weaver::PluginBundle::Author::DOHERTY - Pod::Weaver configuration the way D
 
 =head1 VERSION
 
-version 0.004
+version 0.005
 
 =for Pod::Coverage mvp_bundle_config
 
@@ -67,17 +64,22 @@ The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
 site near you, or see L<http://search.cpan.org/dist/Pod-Weaver-PluginBundle-Author-DOHERTY/>.
 
-The development version lives at L<http://github.com/doherty/Pod-Weaver-PluginBundle-DOHERTY>
-and may be cloned from L<git://github.com/doherty/Pod-Weaver-PluginBundle-DOHERTY.git>.
+The development version lives at L<http://github.com/doherty/Pod-Weaver-PluginBundle-Author-DOHERTY>
+and may be cloned from L<git://github.com/doherty/Pod-Weaver-PluginBundle-Author-DOHERTY.git>.
 Instead of sending patches, please fork this project using the standard
 git and github infrastructure.
+
+=head1 SOURCE
+
+The development version is on github at L<http://github.com/doherty/Pod-Weaver-PluginBundle-Author-DOHERTY>
+and may be cloned from L<git://github.com/doherty/Pod-Weaver-PluginBundle-Author-DOHERTY.git>
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
 Please report any bugs or feature requests through the web interface at
-L<http://github.com/doherty/Pod-Weaver-PluginBundle-Author-DOHERTY/issues>.
+L<https://github.com/doherty/Pod-Weaver-PluginBundle-Author-DOHERTY/issues>.
 
 =head1 AUTHOR
 
@@ -85,11 +87,10 @@ Mike Doherty <doherty@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is Copyright (c) 2010 by Mike Doherty.
+This software is copyright (c) 2010 by Mike Doherty.
 
-This is free software, licensed under:
-
-  The GNU General Public License, Version 3, June 2007
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
